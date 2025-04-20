4fea3580e72934609f75d082c900d309b8643549
louloutte1302 <lou.pepin1@gmail.com>
2025-04-20 12:03:40 +0100

    PÉPIN.PlantesLou.v3:réécriture de Hachich

--- PROJET/Hachich.cs
+++ PROJET/Hachich.cs
@@ -1,4 +1,4 @@
-public class Hachich : Plante
+public class Hachich : Plantes
     {
         private float croissanceActuelle = 0;
         private float age = 0;
@@ -10,36 +10,72 @@ public class Hachich : Plante
             EstComestible = true;
             TerrainPrefere = "terre";
             Espacement = 30;
-            PlaceNecessaire = 0.5;
+            PlaceNecessaire = 0.5f;
             VitesseCroissance = 5;
-            BesoinEau = 2.0;
-            BesoinLumiere = 8.0;
+            BesoinEau = 2.0f;
+            BesoinLumiere = 8.0f;
             TempPreferee = 25;
-            EsperanceDeVie = 16;
-            Rendement = 5;
+            EsperanceDeVie = 30;
+            Fruits = 5;
+            EtatSante = 0.50f;
         }
 
         public override void Pousser(float eau, float lumiere, float temperature, string typeTerrain)
         {
-            float score = 0;
+            
+            if (eau >= BesoinEau) EtatSante += 0.25f;
+            if (lumiere >= BesoinLumiere) EtatSante += 0.25f;
+            if (temperature >= TempPreferee-3 && temperature <= TempPreferee+5) EtatSante += 0.25f;
+            if (typeTerrain == TerrainPrefere) EtatSante += 0.25f;
 
-            if (eau >= BesoinEau) score += 0.25;
-            if (lumiere >= BesoinLumiere) score += 0.25;
-            if (temperature >= TempPreferee-3 && temperature <= ZoneTempPreferee+5) score += 0.25;
-            if (typeTerrain == TerrainPrefere) score += 0.25;
 
-            if (score < 0.5) // si plante inférieure à 50 elle meurt
+            if ((EtatSante < 0.5)|| (age>EsperanceDeVie))// si plante inférieure à 50 elle meurt ou si son espérance de vie est atteinte
             {
-                EtatSante -= 10;
-                if (EtatSante <= 0)
-                {
                     Console.WriteLine($"{Nom} est morte.");
                     return;
-                }
             }
 
-            croissanceActuelle += VitesseCroissance * score;
+            croissanceActuelle += VitesseCroissance * EtatSante;
             age += 2; //on augmente de 2 car saut de deux semaines dans le temps
+
+            if (croissanceActuelle == 0)
+            {
+                Console.WriteLine("Vous venez de planter du HACHICH: . ");
+            }
+
+            if ((croissanceActuelle>0)&&(croissanceActuelle<20))
+            {
+                Console.WriteLine("🌱");
+            }
+
+            if ((croissanceActuelle>20)&&(croissanceActuelle<40))
+            {
+                Console.WriteLine("🌿");
+            }
+
+            if ((croissanceActuelle>40)&&(croissanceActuelle<60))
+            {
+                Console.WriteLine("🥦");
+            }
+
+             if ((croissanceActuelle>40)&&(croissanceActuelle<80))
+            {
+                 Console.WriteLine("C'est la récolte!");
+                 Console.WriteLine("🍃");
+            }
+
+            if ((croissanceActuelle>80)&&(croissanceActuelle<100))
+            {
+                 Console.WriteLine("Vite vite vite!!!!! ");
+                 Console.WriteLine("🍂");
+            }
+
+            if ((croissanceActuelle==100))
+            {
+                 Console.WriteLine("Hachich a fini son cycle de vie");
+                 Console.WriteLine("🪦");
+            }
+
 
             // Simuler une maladie aléatoire
             
--- PROJET/Plantes.cs
+++ PROJET/Plantes.cs
@@ -1,9 +1,9 @@
 public abstract class Plantes
     {
-        public string Nom { get; protected set; }
+        public string? Nom { get; protected set; }
         public bool EstVivace { get; protected set; } // false = annuelle
         public bool EstComestible { get; protected set; }
-        public string TerrainPrefere { get; protected set; } // sable, terre, argile, cailloux
+        public string? TerrainPrefere { get; protected set; } // sable, terre, argile, cailloux
         public float Espacement { get; protected set; } // en cm
         public float PlaceNecessaire { get; protected set; } // en m²
         public float VitesseCroissance { get; protected set; } // en cm par semaine
@@ -12,8 +12,8 @@ public abstract class Plantes
         public float TempPreferee { get; protected set; } // °C
         public float EsperanceDeVie { get; protected set; } // en semaines
         public int Fruits { get; protected set; } // nb de fruits et/ou légumes produits
+        public float EtatSante { get; protected set; } // en pourcentage
         
-
         public abstract void Pousser(float eau, float lumiere, float temperature, string typeTerrain);
         public abstract void Afficher();
     }
