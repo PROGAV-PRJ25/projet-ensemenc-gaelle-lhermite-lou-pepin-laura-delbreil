467927104ff8d67cc891eef4e57c80565cea8ade
louloutte1302 <lou.pepin1@gmail.com>
2025-05-18 11:32:11 +0200

    PÉPIN.main.v10: Console Clear pour propreté

--- Menu.cs
+++ Menu.cs
@@ -15,25 +15,6 @@ public class Menu
 
     NbTerrains = nbTerrainsOptions[nbTerrainsIndex];
 
-
-    // Console.Clear();
-    // Console.WriteLine($"Création de {nbTerrains} terrain(s) de {nbParcelles} parcelle(s) chacun...\n");
-
-    // Random rnd = new Random();
-
-    // for (int i = 0; i < nbTerrains; i++)
-    // {
-    //     string nom = $"T{i + 1}";
-    //     int type = rnd.Next(3);
-    //     Terrain terrain = type switch
-    //     {
-    //         //0 => new TerrainSableux(nom, nbParcellesParTerrain),
-    //         //1 => new TerrainTerreux(nom, nbParcellesParTerrain),
-    //         _ => new TerrainArgileux(nom, nbParcelles)
-    //     };
-    //     terrain.Afficher();
-    //     Console.WriteLine();
-    // }
 }
 
     private int Choisir(string question, string[] options)
@@ -56,7 +37,8 @@ public class Menu
             if (key == ConsoleKey.DownArrow) index = (index + 1) % options.Length;
 
         } while (key != ConsoleKey.Enter);
-
+        Console.Clear();
         return index;
+        
     }
 }
